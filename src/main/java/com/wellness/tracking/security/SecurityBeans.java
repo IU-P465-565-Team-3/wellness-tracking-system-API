308c7a17f6ce7d2f02f5248b782ce0662d750ef1
Amber Ramesh <amramesh@iu.edu>
2021-10-23 16:54:10 -0400

    Fix CORS usage and cookie attributes for auth - Add additional properties for the auth cookie - Set AllowCredentials header to accept request cookies - Apply CORS header for specified origins

--- src/main/java/com/wellness/tracking/security/SecurityBeans.java
+++ src/main/java/com/wellness/tracking/security/SecurityBeans.java
@@ -8,12 +8,18 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
+
 @Component
 public class SecurityBeans {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080", "http://localhost:8085", "https://iub-wellness-tracker.netlify.app"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        source.registerCorsConfiguration("/**", configuration);
         return source;
     }
 
