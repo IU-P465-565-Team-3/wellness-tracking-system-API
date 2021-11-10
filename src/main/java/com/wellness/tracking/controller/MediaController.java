893dff88f6406cf12401da050ab8bf59d167efa1
Amber Ramesh <amramesh@iu.edu>
2021-11-10 01:45:38 -0500

    Remove Java 11 target, switch usage of Path to Paths

--- src/main/java/com/wellness/tracking/controller/MediaController.java
+++ src/main/java/com/wellness/tracking/controller/MediaController.java
@@ -12,6 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.UUID;
 
 @RestController
@@ -25,10 +26,9 @@ public class MediaController {
         try {
             String tmpDir = System.getProperty("java.io.tmpdir");
             String fileName = UUID.randomUUID().toString().replace("-", "");
-            Path filePath = Path.of(tmpDir, fileName);
-            File tmpFile = new File(filePath.toString());
-            file.transferTo(tmpFile);
-            s3Repository.uploadObject(fileName, tmpFile);
+            Path filePath = Paths.get(tmpDir, fileName);
+            file.transferTo(filePath);
+            s3Repository.uploadObject(fileName, filePath.toFile());
             return new ResponseEntity<>(fileName, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>("Unable to upload.", HttpStatus.BAD_REQUEST);
